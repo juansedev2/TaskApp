277c2881464ed0777310492b5144941516d0fb8c
Juanse <jsab62tol@gmail.com>
2022-11-16 16:12:30 -0500

    First commit of my App

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App(){
+  return(<h1>Aplicación de tareas en React</h1>);
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/App.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+//!React.StrictMode es un componente que ayuda a mostrar errores mientras se está programando dentro del componente hijo
+//!así entonces es una buena práctica usarlo
